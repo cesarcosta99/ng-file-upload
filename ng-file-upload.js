2c4ae6428b7075c767821d14fbdf201d818e60e7
César Costa <costa.cesar.a@hotmail.com>
2016-09-23 01:04:15 -0300

    File upload setup

--- ng-file-upload.js
+++ ng-file-upload.js
@@ -11,9 +11,7 @@
         link: link,
         restrict: 'E',
         replace: true,
-        scope: {
-          
-        }
+        scope: true
     };
 
     return directive;
@@ -23,11 +21,16 @@
 
       angular.element('#upload-input').fileupload({
         dataType: 'json',
+        formData: {
+          api_password: 'b77c7a89f6a0be676526fff0248baa4b4583b26f1c4d828b2336cf7010e780e6'
+        },
         add: function (event, data) {
           scope.progress = 0;
+
           data.submit();
         },
         done: function (event, data) {
+          console.log(data);
         },
         progressall: function (event, data) {
           scope.$apply(function () {
