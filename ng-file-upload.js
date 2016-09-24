375387f8d01c5a2ba6146aa5ff563bbccf3a4e3c
César Costa <costa.cesar.a@hotmail.com>
2016-09-24 00:08:40 -0300

    Fixed variable call; Fixed timeout; Added some style.

--- ng-file-upload.js
+++ ng-file-upload.js
@@ -18,6 +18,7 @@
 
     function link(scope, element, attrs) {
       var api_password = 'b77c7a89f6a0be676526fff0248baa4b4583b26f1c4d828b2336cf7010e780e6';
+      scope.embedUrl = '';
       scope.progress = 0;
 
       angular.element('#upload-input').fileupload({
@@ -46,12 +47,13 @@
 
       function loadVideo(hashed_id) {
         $http.get('https://api.wistia.com/v1/medias/' + hashed_id + '.json?api_password=' + api_password).then(function (response) {
-          console.log(response.data);
           if (response.data.status === 'ready') {
-            scope.embedUrl = $sce.trustAsResourceUrl('http://fast.wistia.net/embed/iframe/' + hashed_id);
+            scope.embedUrl = $sce.trustAsResourceUrl('https://fast.wistia.net/embed/iframe/' + hashed_id);
           } else {
             if (response.data.status !== 'failed') {
-              $timeout(loadVideo(hashed_id), 5000);
+              $timeout(function () {
+                loadVideo(hashed_id)
+              }, 5000);
             }
           }
         });
